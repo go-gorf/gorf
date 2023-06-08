32950787babde5d2b305388c86286515d8c4b929
Tom Victor <vjtomvictor@icloud.com>
2023-06-08 21:58:47 +0200

    add go admin example

---
+++ examples/admin/settings.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-gorf/admin"
+	"github.com/go-gorf/gorf"
+)
+
+var apps = []gorf.GorfApp{
+	&admin.AdminApp,
+}
+
+func LoadSettings() {
+	// jwt secret key
+	gorf.Settings.SecretKey = "GOo8Rs8ht7qdxv6uUAjkQuopRvjndsvnsn08YleBx6pEv0cQ09a"
+
+}
+
+// BootstrapRouter bootstrap server
+func BootstrapRouter() *gin.Engine {
+	gorf.Apps = append(apps)
+	LoadSettings()
+	_ = gorf.InitializeDatabase()
+
+	gorf.SetupApps()
+	r := gin.Default()
+	gorf.RegisterApps(r)
+	return r
+}
